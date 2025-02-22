6a480b56f61581c4a1972462215cfc3dd3f8cfa2
AzoN2525 <diogo.l.oliveira@ua.pt>
2025-02-22 11:03:54 +0000

    2.1 done

--- lab2/lab2_1stock/src/main/java/com/example/StocksPortfolio.java
+++ lab2/lab2_1stock/src/main/java/com/example/StocksPortfolio.java
@@ -20,4 +20,28 @@ public class StocksPortfolio{
         return value;
     }
 
+    public List<Stock> mostValuableStocks(int topN) {
+
+        if(topN <= 0) {
+            return new ArrayList<Stock>();
+        }
+
+        List<Stock> mostValuable = new ArrayList<Stock>();
+        List<Stock> copy = new ArrayList<Stock>(stocks);
+
+        for (int i = 0; i < topN; i++) {
+            Stock max = copy.get(0);
+            for (Stock stock : copy) {
+                if (stockMarket.lookUpPrice(stock.getLabel()) * stock.getQuantity() > stockMarket.lookUpPrice(max.getLabel()) * max.getQuantity()) {
+                    max = stock;
+                }
+            }
+            mostValuable.add(max);
+            copy.remove(max);
+        }
+
+        return mostValuable;
+        
+    }
+
 }
--- lab2/lab2_1stock/src/test/java/StockPortfolioTest.java
+++ lab2/lab2_1stock/src/test/java/StockPortfolioTest.java
@@ -13,6 +13,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+// import org.hamcrest.*;
+
 import com.example.IStockMarketService;
 import com.example.Stock;
 import com.example.StocksPortfolio;
@@ -33,7 +35,7 @@ public class StockPortfolioTest {
         // Load the mock with the proper expectations
         when(stockMarket.lookUpPrice("EBAY")).thenReturn(4.0);
         when(stockMarket.lookUpPrice("MSFT")).thenReturn(3.0);
-        when(stockMarket.lookUpPrice("GOOGL")).thenReturn(5.0);  // Will not be used
+        // when(stockMarket.lookUpPrice("GOOGL")).thenReturn(5.0);  // Will not be used
         
         // Execute the tested method
         portfolio.addStock(new Stock("EBAY", 2));
@@ -46,6 +48,30 @@ public class StockPortfolioTest {
         verify(stockMarket, times(2)).lookUpPrice(anyString());
     }
 
-    
 
+    @Test  
+    void topNvaluableStocks(){
+            
+        // Load the mock with the proper expectations
+        when(stockMarket.lookUpPrice("EBAY")).thenReturn(4.0);
+        when(stockMarket.lookUpPrice("MSFT")).thenReturn(3.0);
+        when(stockMarket.lookUpPrice("GOOGL")).thenReturn(5.0);  // Will not be used
+
+        // Execute the tested method
+        portfolio.addStock(new Stock("EBAY", 2));
+        portfolio.addStock(new Stock("MSFT", 3));
+        portfolio.addStock(new Stock("GOOGL", 1));
+        portfolio.addStock(new Stock("GOOGL", 2));
+        portfolio.addStock(new Stock("FB", 3));
+        portfolio.addStock(new Stock("AMZN", 1));
+        portfolio.addStock(new Stock("AAPL", 2));
+
+        // Verify the result and the use of the mock
+        assertEquals(3, portfolio.mostValuableStocks(3).size(), "mostValuableStocks: wrong number of stocks returned");
+        assertEquals(0, portfolio.mostValuableStocks(0).size(), "mostValuableStocks: wrong number of stocks returned");
+        assertEquals(0, portfolio.mostValuableStocks(-1).size(), "mostValuableStocks: wrong number of stocks returned");
+        assertEquals(7, portfolio.mostValuableStocks(7).size(), "mostValuableStocks: wrong number of stocks returned");
+    }
+
+    
 }
